97c06862949a6523b51d56a7b26ee7f71ac5898c
zacharilius <zabensley@gmail.com>
2015-07-27 16:17:41 -0700

    Started NewsFeedApp

--- TwitchApp/twitchApp.js
+++ TwitchApp/twitchApp.js
@@ -29,20 +29,26 @@ $(document).ready(function(){
   function getUsername(user){
     $.ajax({
         url:
-          "https://api.twitch.tv/kraken/channels/" + user,
+          "https://api.twitch.tv/kraken/streams/" + user,
         dataType: "jsonp",
         success: function(parsed_json) {
-          var username = parsed_json.display_name;
-          var status = parsed_json.status; // The status of the user
-          var logo = parsed_json.logo;
-          var url = parsed_json.url;
+          var streaming = parsed_json.stream; // The status of the user
+
+
+
+
           console.log(parsed_json);
-          if(status !== null){
-            var html = '<a href = + ' + url + '><div class="row userProfiles"><div class="col-sm-3"><img class="logo" alt = "No Profile Pic" src=' + logo + '></div><div class="col-sm-6"><p>'+ username +'</p><p class = "projectText">'+ status + '</p></div><div class="col-sm-3"><span class="glyphicon glyphicon-ok" style="color:green" aria-hidden="true"></span> Online</div></div></a>';
+          if(streaming !== null){
+            var status = parsed_json.stream.game;
+            var url = parsed_json._links.channel;
+            var username = parsed_json.stream.channel.display_name;
+            var logo = parsed_json.stream.channel.logo;
+            var html = '<a href = ' + url + '><div class="row userProfiles"><div class="col-sm-3"><img class="logo" alt = "No Profile Pic" src=' + logo + '></div><div class="col-sm-6"><p>'+ username +'</p><p class = "projectText">'+ status + '</p></div><div class="col-sm-3"><span class="glyphicon glyphicon-ok" style="color:green" aria-hidden="true"></span> Online</div></div></a>';
             $("#usernames").prepend(html);
           }
           else{
-            var html = '<a href = + ' + url + '><div class="row userProfiles loggedOut"><div class="col-sm-3"><img class="logo" alt = "No Profile Pic" src=' + logo + '></div><div class="col-sm-6"><p>'+ username +'</p></div><div class="col-sm-3"><span class="glyphicon glyphicon-remove	" style="color:red" aria-hidden="true"></span> Offline</div></div></a>'
+            logo = "";
+            var html = '<div class="row userProfiles loggedOut"><div class="col-sm-3"><img class="logo" alt = "No Profile Pic" src=' + logo + '></div><div class="col-sm-6"><p>'+ user +'</p></div><div class="col-sm-3"><span class="glyphicon glyphicon-remove	" style="color:red" aria-hidden="true"></span> Offline</div></div>'
             $("#usernames").append(html);
           }
           console.log(html);
