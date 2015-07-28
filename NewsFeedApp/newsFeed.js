4f553b28f479bf6261be8ba9de4d1149827d5484
zacharilius <zabensley@gmail.com>
2015-07-28 14:08:04 -0700

    Completed NewsFeedApp

--- NewsFeedApp/newsFeed.js
+++ NewsFeedApp/newsFeed.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
+  getAJAX();
 
+
+  function getAJAX(){
+    $.ajax({
+        url:
+          "http://www.freecodecamp.com/stories/hotStories",
+        success: function(parsed_json) {
+          for(var elem in parsed_json){
+            console.log("yep");
+            var username = parsed_json[elem].author.username;
+            var picture = parsed_json[elem].author.picture;
+            var headline = parsed_json[elem].headline;
+            var link = parsed_json[elem].link;
+            var comments = parsed_json[elem].comments.length;
+
+            var html =
+              '<div class="col-width"><a href="' + link + '" class="thumbnail"><img src="' + picture + '" alt="Article Pic"><div class="caption"><p>' +headline + '</p><p>' + username + '</p><p>Comments: ' + comments + '</p></a></div>';
+            console.log(html);
+            $('#stories').append(html);
+        }
+  }
 })
+}
+});
